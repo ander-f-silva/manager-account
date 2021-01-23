4e08e6d388620685b02ff2ff149a555da4db55e1
andferreira <anderson.ferreira@mercadolivre.com>
2021-01-22 21:15:31 -0300

    [Struct Project] add archifectories the manager and organizated the project

---
+++ src/test/java/br/com/db/mg/ManagerAccountTest.java
@@ -0,0 +1,21 @@
+package br.com.db.mg;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class ManagerAccountTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
