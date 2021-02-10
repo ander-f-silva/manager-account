2de3e4d286cc5e14f656a03593f3604be8df9d99
andferreira <anderson.ferreira@mercadolivre.com>
2021-02-10 12:30:04 -0300

    [Endpoint Transfer] Add new test to this endpoint

--- src/test/java/br/com/dn/mg/account/application/AccountsResourceTest.java
+++ src/test/java/br/com/dn/mg/account/application/AccountsResourceTest.java
@@ -148,4 +148,43 @@ class AccountsResourceTest {
             assertEquals(statusCode, httpClientResponseException.getStatus().getCode());
         }
     }
+
+    @DisplayName("should perform transfer entry accoutn. cases: ")
+    @ParameterizedTest(name = "{0}")
+    @CsvSource({
+            "return the transfer perform with success, true, 2000, 200",
+           //"return error to account of origen not found, true, 404",
+           // "return error to account of destination not found, true, 404",
+          //  "return to account of origin without balance, true, 422",
+         //   "return bad request to account of destination, true, 400",
+         //   "return bad request to value not found, true, 400",
+    })
+    void testTransferEntryAccounts(ArgumentsAccessor arguments) {
+        Boolean accountExist =  Boolean.valueOf(arguments.get(1).toString());
+        Double value =  Double.valueOf(arguments.get(2).toString());
+        Integer statusCode =  Integer.valueOf(arguments.get(3).toString());
+
+        try {
+            UUID toAccountId = UUID.randomUUID();
+            AccountEntity toAccount = new AccountEntity();
+
+            UUID fromAccountId = UUID.randomUUID();
+            AccountEntity fromAccount = new AccountEntity();
+
+            if (accountExist) {
+                var  optAccount = accountRepository.findByDocument("39670899087");
+                toAccount = optAccount.get();
+                toAccountId = toAccount.getId();
+
+                optAccount = accountRepository.findByDocument("39670899087");
+                fromAccount = optAccount.get();
+                fromAccountId = fromAccount.getId();
+            }
+
+            HttpResponse<?> response = client.toBlocking().exchange(HttpRequest.PATCH("/accounts/" + toAccount.toString() + "/transfer",  new DepositAccountDTO(value)), NewAccountDTO.class);
+            assertEquals(statusCode, response.getStatus().getCode());
+        } catch (HttpClientResponseException httpClientResponseException) {
+            assertEquals(statusCode, httpClientResponseException.getStatus().getCode());
+        }
+    }
 }
